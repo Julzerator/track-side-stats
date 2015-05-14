5cd566f5f8c15384917f7f8c32949e9e90c2e126
Julie Haehn <julie_funko@Julies-MacBook-Pro.local>
2015-05-14 14:54:47 -0700

    Added task tracking. Installed and hooked up PostgreSQL

--- server.py
+++ server.py
@@ -5,11 +5,17 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, LeagueUser, Team, Player,
+    Position, TeamPlayer, League, PersonPosition, Roster, RosterPlayer,
+    Game, Jam, JamPosition, Action
 from datetime import datetime
 
 
 app = Flask(__name__)
+
+app.config['SQL_ALCHEMY_DATABASE_URL'] = \
+    'postgresql://localhost/trackstats.db'
+
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
@@ -21,13 +27,55 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    """Homepage."""
+    """
+    Homepage.
+    """
 
+
+    render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    """
+    A User requests an account.
+    This is not fully realized for demo purposes.
+    """
+
+    return pass # I want to return their info then
+                # email them a link in the final version.
+
+
+@app.route('/login')
+def login():
+    """
+    Go to the login page if no post information.
+    User logs in to the site.
+    """
 
     return pass
 
 
+@app.route('/logout')
+def logout():
+    """
+    User logs out of the site.
+    """
 
+    return pass
+
+
+@app.route('/account')
+def account_update():
+    """
+    Go to the account page if no post information.
+    Otherwise update the account of current User.
+    """
+
+    return pass
+
+
+@app.route('/league')
 
 
 if __name__ == "__main__":
