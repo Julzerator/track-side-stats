a5a8f3515762f0414c79dd00958718f51818bf93
Julie Haehn <julie_funko@Julies-MBP.midgard.net>
2015-06-10 01:51:33 -0700

    Turned off debug

--- server.py
+++ server.py
@@ -1111,7 +1111,7 @@ def clear_session():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
